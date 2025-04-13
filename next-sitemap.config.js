0fb5cfedf131779f44d5ac7046f59ef5141aa75f
Almash <almashcarrental@gmail.com>
2025-04-14 00:39:56 +0530

    googlesite changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://goadrive.com', // 🔁 Replace with your actual domain
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+  }
+  
